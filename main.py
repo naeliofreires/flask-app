ebe8bf9d5649a5be86588f825e4e981a8e67084d
Naélio Freires <naeliosistemas@gmail.com>
2024-12-31 10:51:18 -0300

    creating sq-lite database and tables

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+# SQLAlchemy
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy_utils import database_exists, create_database
 
-# App
+# Initialize Flask App
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///store.db"
 
-# DB
+# Database Base Class
 class Base(DeclarativeBase):
   pass
 
+# Initialize SQLAlchemy
 db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# Models
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(length=12), nullable=False, unique=True)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
-db.init_app(app) # initialize the app with the extension
+    def __repr__(self):
+        return f"Item('{self.id}', '{self.name}', '{self.code}', '{self.price}')"
+
+# Create Database
+engine = create_engine("sqlite:///store.db")
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+with app.app_context():
+    # db.drop_all()
+    db.create_all()
+
+# Database Status
+print(f'DB is created: {database_exists(engine.url)}')
+print(f'DB: {engine.url}')
 
 # Routes
 @app.route('/') 
